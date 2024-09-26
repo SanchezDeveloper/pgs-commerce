e7e18f53761c7e3855e826db3626c4a09b4ee6b9
SanchezDeveloper <vinicsanchez@gmail.com>
2024-09-25 21:07:27 -0300

    adicionando tela de Loading e configurando Stripe para produtos carreagar produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,37 @@
 import { ProductType } from "@/types/ProductType";
 import Product from "./components/Product";
+import Stripe from "stripe";
 
 
-async function getProducts() {
-  const res = await fetch('https://fakestoreapi.com/products');
+async function getProducts(): Promise<ProductType[]> {
+
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    apiVersion: '2024-06-20'
+  });
+
+  const products = await stripe.products.list();
+  const formatedProducts = await Promise.all(
+    products.data.map(async (product) => {
+      const price = await stripe.prices.list({
+        product: product.id,
+      });
+      return {
+        id: product.id,
+        price: price.data[0].unit_amount,
+        name: product.name,
+        image: product.images[0],
+        description: product.description,
+        currency: price.data[0].currency,
+      }
+    })
+  );
+
+  return formatedProducts;
+  /** const res = await fetch('https://fakestoreapi.com/products');
   if (!res.ok) {
     throw new Error('Falha ao buscar os dados');
   }
-  return res.json();
+  return res.json(); **/
 }
 
 export default async function Home() {
@@ -19,7 +43,7 @@ export default async function Home() {
         className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6
         '
         >
-          {products.map((product: ProductType) => (
+          {products.map((product) => (
             <Product key={product.id} product={product}></Product>
           ))}
         </div>
