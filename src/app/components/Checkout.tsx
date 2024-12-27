8e9f0b4a8917f332e620a37b08e76b4c00df60a1
SanchezDeveloper <vinicsanchez@gmail.com>
2024-12-27 12:40:57 -0300

    Stripe webhooks, but apis pendings

--- src/app/components/Checkout.tsx
+++ src/app/components/Checkout.tsx
@@ -1,70 +1,9 @@
-// 'use client'
-// import { loadStripe, StripeElementsOptions} from "@stripe/stripe-js"
-// import { Elements, PaymentElement } from "@stripe/react-stripe-js"
-// import { useCartStore } from "@/store";
-// import { useEffect, useState } from "react";
-
-// const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
-
-// export default function Checkout() {
-//     const cartStore = useCartStore();
-//     const [clientSecret, setClientSecret] = useState('');
-
-//     useEffect(() => {
-//         fetch('/api/create-payment-intent', {
-//             method: 'POST',
-//             headers: { 
-//                 "Content-Type": "application/json",
-//             },
-//             body: JSON.stringify({ 
-//                 items: cartStore.cart,
-//                 payment_intent_id: cartStore.paymentIntent,
-//             }),
-//         }).then((res) => {
-//             console.log("Resposta da API:", res);
-//             return res.json();
-//         }).then((data) => {
-//             console.log("Dados retornados:", data);
-//             cartStore.setPaymentIntent(data.paymentIntent.id);
-//             setClientSecret(data.paymentIntent?.client_secret);
-//         }).catch((error) => {
-//             console.error("Erro ao fazer fetch:", error);
-//         });
-//     }, [cartStore, cartStore.cart, cartStore.paymentIntent]);
-    
-//     const options: StripeElementsOptions = {
-//         clientSecret,
-//         appearance: {
-//             theme: 'night',
-//             labels: 'floating'
-//         }
-//     }
-
-//     return ( 
-//         <div>
-//             {
-//                 clientSecret ? (
-//                     <Elements options={options} stripe={stripePromise}>
-//                         < PaymentElement id="payment-element" options={{ layout: 'tabs' }}/>
-//                     </Elements>
-                    
-                    
-//                 ) : (
-//                     <div>
-//                         <h1>Carregando...</h1>
-//                     </div>
-//                 )
-//             }
-            
-//         </div>
-//     );
-// }
-
 'use client'
-import { loadStripe, StripeElementsOptions } from "@stripe/stripe-js"
+import { loadStripe, StripeElementsOptions} from "@stripe/stripe-js"
 import { Elements, PaymentElement } from "@stripe/react-stripe-js"
 import { useCartStore } from "@/store";
 import { useEffect, useState } from "react";
+import CheckoutForm from "./CheckoutForm";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
@@ -82,33 +21,24 @@ export default function Checkout() {
                 items: cartStore.cart,
                 payment_intent_id: cartStore.paymentIntent,
             }),
-        })
-        .then((res) => {
-            // Primeiro, obtém a resposta como texto
-            return res.text();
-        })
-        .then((responseText) => {
-            console.log("Resposta da API:", responseText);
-            try {
-                // Tenta converter o texto para JSON
-                const data = JSON.parse(responseText);
-                console.log("Dados retornados:", data);
-
-                // Verifica se os dados possuem a estrutura esperada
-                if (data.paymentIntent) {
-                    cartStore.setPaymentIntent(data.paymentIntent.id);
-                    setClientSecret(data.paymentIntent?.client_secret);
-                } else {
-                    console.error("Dados do paymentIntent não encontrados.");
-                }
-            } catch (error) {
-                console.error("Erro ao processar a resposta JSON:", error);
-            }
-        })
-        .catch((error) => {
-            console.error("Erro ao fazer fetch:", error);
+         }).then((res) => { //return res.json() }).then((data) => {
+        //     cartStore.setPaymentIntent(data.paymentIntent.id);
+        //     console.log('data.paymentIntent', data.paymentIntent)
+        //     setClientSecret(data.paymentIntent?.client_secret)
+        // })
+            console.log("Resposta da API:", res);
+            return res.json();
+        }).then((data) => {
+            console.log("Dados retornados:", data);
+            cartStore.setPaymentIntent(data.paymentIntent.id);
+            setClientSecret(data.paymentIntent?.client_secret);
+        }).catch((error) => {
+            console.error("Erro ao criar Payment Intent:", error);
         });
     }, [cartStore, cartStore.cart, cartStore.paymentIntent]);
+    
+    console.log("CartStore:", cartStore.cart);
+    console.log("PaymentIntent:", cartStore.paymentIntent);
 
     const options: StripeElementsOptions = {
         clientSecret,
@@ -123,16 +53,16 @@ export default function Checkout() {
             {
                 clientSecret ? (
                     <Elements options={options} stripe={stripePromise}>
-                        < PaymentElement id="payment-element" options={{ layout: 'tabs' }}/>
-                    </Elements>
-                    
-                    
+                        <CheckoutForm clientSecret={clientSecret}/>
+                    </Elements>              
                 ) : (
                     <div>
                         <h1>Carregando...</h1>
                     </div>
                 )
             }
+            
         </div>
     );
 }
+
